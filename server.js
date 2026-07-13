9dfad5691d7831570c29a8c261560cc84f8b4f22
Quincy-Bee <quincybee@icloud.com>
2026-07-12 22:26:56 -0400

    routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import seedRoute from "./routes/seed.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 
 connectDB();
+
+app.use("/seed", seedRoute);
 
 app.get("/", (req, res) => {
   res.send("SBA 319 MongoDB Database Application API");
