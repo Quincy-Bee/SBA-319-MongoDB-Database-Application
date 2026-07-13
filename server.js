cdcdbaba8d011a838d75cd1c1399d5ce070461da
Quincy-Bee <quincybee@icloud.com>
2026-07-12 22:05:40 -0400

    getting server to run

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("SBA 319 MongoDB Database Application API");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
